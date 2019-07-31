31dee510509f65f9dea1ed4a6a34e806fc81ddc0
Hyeonjiwon <zlone1208@gmail.com>
2019-07-31 23:26:44 +0900

    second commit

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 ###DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -125,3 +126,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
